4c078fd38790883e6ed5b6348c840867c46e1755
SJ Han <62943439+stat1202@users.noreply.github.com>
2024-11-29 13:12:16 +0900

    이진 탐색

---
+++ binary_search.js
@@ -0,0 +1,26 @@
+function binarySearch_loop(arr, n) {
+  let lowIndex = 0;
+  let midIndex = 0;
+  let highIndex = arr.length - 1;
+
+  while (lowIndex <= highIndex) {
+    midIndex = Math.floor((lowIndex + highIndex) / 2);
+
+    if (arr[midIndex] > n) {
+      highIndex = midIndex - 1;
+    } else if (arr[midIndex] < n) {
+      lowIndex = midIndex + 1;
+    } else {
+      return midIndex;
+    }
+  }
+
+  return -1;
+}
+
+let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(binarySearch_loop(array, 0));
+console.log(binarySearch_loop(array, 3));
+console.log(binarySearch_loop(array, 7));
+console.log(binarySearch_loop(array, 10));
